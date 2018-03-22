7797bd8ac459f38b5600b3c24f1959d1ee0d7752
kdr154 <rosenkranz@uni-tuebingen.de>
2018-03-22 11:56:19 +0100

    change

--- PicaSWB_batch.js
+++ PicaSWB_batch.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 2,
 	"browserSupport": "gcs",
-	"lastUpdated": "2018-03-22 15:46:00"
+	"lastUpdated": "2018-03-22 11:50:00"
 }
 
 
--- PicaSWBixzs.js
+++ PicaSWBixzs.js
@@ -9,7 +9,7 @@
 	"inRepository": true,
 	"translatorType": 2,
 	"browserSupport": "gcs",
-	"lastUpdated": "2017-08-10 09:27:00"
+	"lastUpdated": "2018-03-22 09:27:00"
 }
 
 
