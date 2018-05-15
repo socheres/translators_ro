4dfd23c35461f8e43a9fcc68d67c6cacd01bd77b
kdr154 <rosenkranz@uni-tuebingen.de>
2018-05-15 09:00:45 +0200

    CScR

--- PicaSWB_batch.js
+++ PicaSWB_batch.js
@@ -306,6 +306,7 @@ var journalMapping = {
 	"1862-5886" : "!281314136!", // Zeitschrift für junge Religionswissenschaft
 	"1612-2941" : "!281189013!", // Marburg Journal of Religion
 	"0017-2251" : "!015194361!", // Christian scholar’s review
+	"00172251" : "!015194361!", // Christian scholar’s review
 	"0774-5524" : "!015182746!", // Questions liturgiques
 	"1861-5813" : "!281289085!", // Online - Heidelberg Journal of Religions on the Internet
 	"1783-1806" : "!112891241!", // Studies in Interreligious Dialogue
@@ -1371,6 +1372,7 @@ var issnPhysicalFormMapping = {
 	"1862-5886" : "O", // Zeitschrift für junge Religionswissenschaft
 	"1612-2941" : "O", // Marburg Journal of Religion
 	"0017-2251":  "A", // Christian scholar’s review
+	"00172251":  "A", // Christian scholar’s review
 	"0774-5524" : "A", // Questions liturgiques
 	"1861-5813" : "O", // Online - Heidelberg Journal of Religions on the Internet
 	"1783-1806" : "O", // Studies in Interreligious Dialogue
